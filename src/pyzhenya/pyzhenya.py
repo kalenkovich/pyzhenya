f3baa1a0f13682e097bb36e85699169bc494634c
Zhenya Kalenkovich <e.kalenkovich@gmail.com>
2024-02-03 15:47:19 -0500

    docs: create numpydoc docstrings

--- src/pyzhenya/pyzhenya.py
+++ src/pyzhenya/pyzhenya.py
@@ -7,7 +7,26 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def text_to_image(text: str) -> Image.Image:
-    """Creates a PIL image object with the given text, transparent background, and minimal padding."""
+    """Creates a PIL image object with the given text.
+
+    The image will have transparent background and minimal padding and will be in the "RGBA" mode.
+
+    Parameters
+    ----------
+    text : str
+        The text to convert into an image
+
+    Returns
+    -------
+    Image.Image
+        The image object
+
+    Examples
+    --------
+    >>> from pyzhenya.pyzhenya import text_to_image
+    >>> image = text_to_image("😍")
+    >>> image.show()
+    """
     font = ImageFont.truetype('seguiemj.ttf', size=250, encoding='unic')
     (left, top, right, bottom) = font.getbbox(text)
     text_width = right - left
@@ -24,5 +43,25 @@ def text_to_image(text: str) -> Image.Image:
 
 
 def text_to_image_file(text: str, output_path: Union[str | Path]) -> None:
-    """Creates an image file with the given text, transparent background, and minimal padding."""
+    """Creates an image file with the given text.
+
+    The image will have transparent background and minimal padding. The output format will be inferred by PIL from
+    the output file name.
+
+    Parameters
+    ----------
+    text : str
+        The text to convert into an image
+    output_path : str | Path
+        The output path for the image file
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> from pyzhenya.pyzhenya import text_to_image_file
+    >>> text_to_image_file("😍", "heart-eyes.png")
+    """
     text_to_image(text).save(output_path)
